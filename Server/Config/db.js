e2f906754be6f835fec91a29a2b291ec0a74b081
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-10-04 16:39:56 -0400

    updated db config

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/store";
-exports.RemoteURI = "mongodb+srv://thomas:axftHWWcFTfbhoTq@comp229-f2021.zvhmy.mongodb.net/store?retryWrites=true&w=majority";
+exports.RemoteURI = process.env.RemoteURI;
 exports.HostName = "remotehost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/store";
-export const RemoteURI = "mongodb+srv://thomas:axftHWWcFTfbhoTq@comp229-f2021.zvhmy.mongodb.net/store?retryWrites=true&w=majority";
+export const RemoteURI = process.env.RemoteURI;
 export const HostName = "remotehost";
 export const Secret = "someSecret";
