ca9cc72bc35fa7546b328d066a6081e9b0c548d3
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-10-18 17:11:02 -0400

    added Login / Register capability

--- Server/Controllers/index.ts
+++ Server/Controllers/index.ts
@@ -1,5 +1,10 @@
 import express, {Request, Response, NextFunction} from 'express';
 
+import passport from 'passport';
+
+// create an instance of the User Model
+import User from '../Models/user';
+
 // get a reference to the Game Model Class
 import Game from '../Models/game';
 
@@ -45,12 +50,89 @@ export function DisplayGamesListPage(req: Request, res: Response, next: NextFunc
   });
 }
 
+/* functions for authentication */
+
 export function DisplayLoginPage(req: Request, res: Response, next: NextFunction): void
 {
     res.render('index', { title: 'Login', page: 'login'  });
 }
 
+export function ProcessLoginPage(req: Request, res: Response, next: NextFunction): void
+{
+  passport.authenticate('local', (err, user, info) =>
+  {
+    console.log(user);
+
+    // are there any server errors?
+    if(err)
+    {
+      console.error(err);
+      return next(err);
+    }
+
+    // are there any login errors?
+    if(!user)
+    {
+      req.flash('loginMessage', 'Authentication Error');
+      return res.redirect('/login');
+    }
+
+    req.login(user, (err) => 
+    {
+      // are there any db errors?
+      if(err)
+      {
+        console.error(err);
+        return next(err);
+      }
+
+      console.log("Logged in Successfully");
+
+      return res.redirect('/games-list');
+    });
+  })(req, res, next);
+}
+
 export function DisplayRegisterPage(req: Request, res: Response, next: NextFunction): void
 {
     res.render('index', { title: 'Register', page: 'register'  });
+}
+
+export function ProcessRegisterPage(req: Request, res: Response, next: NextFunction): void
+{
+  // instantiate a new User Object
+  let newUser = new User
+  ({
+    username: req.body.username,
+    emailAddress: req.body.emailAddress,
+    displayName: req.body.firstName + " " + req.body.lastName
+  });
+
+  User.register(newUser, req.body.password, (err) => 
+  {
+    if(err)
+    {
+      console.error('Error: Inserting New User');
+      if(err.name == "UserExistsError")
+      {
+        console.error('Error: User Already Exists');
+      }
+      req.flash('registerMessage', 'Registration Error');
+
+      return res.redirect('/register');
+    }
+
+    // after successful registration - let's login the user
+    return passport.authenticate('local')(req, res, () =>
+    {
+      return res.redirect('/games-list');
+    });
+  });
+}
+
+export function ProcessLogoutPage(req: Request, res: Response, next: NextFunction): void
+{
+  req.logOut();
+
+  res.redirect('/login');
 }
--- Server/Routes/index.ts
+++ Server/Routes/index.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 export default router;
 
 // instantiate an object of type index controller
-import {DisplayAboutPage, DisplayContactPage, DisplayGamesListPage, DisplayHomePage, DisplayLoginPage, DisplayProjectsPage, DisplayRegisterPage, DisplayServicesPage } from '../Controllers/index';
+import {DisplayAboutPage, DisplayContactPage, DisplayGamesListPage, DisplayHomePage, DisplayLoginPage, DisplayProjectsPage, DisplayRegisterPage, DisplayServicesPage, ProcessLoginPage, ProcessLogoutPage, ProcessRegisterPage } from '../Controllers/index';
 
 /* GET home page. */
 router.get('/', DisplayHomePage);
@@ -26,11 +26,20 @@ router.get('/contact', DisplayContactPage);
 /* GET games-list */
 router.get('/games-list', DisplayGamesListPage);
 
-/* GET login page */
+/* GET display login page */
 router.get('/login', DisplayLoginPage);
 
-/* GET register page */
+/* POST process login page */
+router.post('/login', ProcessLoginPage);
+
+/* GET display register page */
 router.get('/register', DisplayRegisterPage);
+
+/* POST process register page */
+router.post('/register', ProcessRegisterPage);
+
+/* GET logout page */
+router.get('/logout', ProcessLogoutPage);
 
 
 //module.exports = router;
